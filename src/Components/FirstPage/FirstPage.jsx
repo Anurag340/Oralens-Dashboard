9502a5a911fbb7c77175c3100e6c74d0aebad4ab
Anurag <anuragsamal123@gmail.com>
2025-01-21 14:45:03 +0530

    Two way binding done

--- src/Components/FirstPage/FirstPage.jsx
+++ src/Components/FirstPage/FirstPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './FirstPage.css'
 
 const FirstPage = () => {
     const navigate = useNavigate();
+    const [login,setlogin] = useState('');
+    const [orgname,setorgname] = useState('');
+    const [orgloc,setorgloc] = useState('');
+    const [data,setdata] = useState({});
+    const handlesubmit = (e) => {
+        e.preventDefault();
+
+        setdata({
+            login: login,
+            orgname: orgname,
+            orgloc: orgloc
+        });
+        setlogin('');
+        setorgname('');
+        setorgloc('');
+        console.log(data);
+        navigate('/login');
+    }
   return (
     <div className='firstpage h-screen w-full flex flex-col relative '>
         <p className=' brand absolute text-white text-lg left-[3vw] top-[3vw] '>Oralens</p>
@@ -16,16 +34,16 @@ const FirstPage = () => {
             <div className='bar h-[80%] w-[1px] bg-white ' ></div>
 
             <div className=' formdiv w-[50%] h-full flex justify-center items-center ' >
-                <form className=' formy h-[80%] w-[70%] bg-blue-900 rounded-xl flex flex-col gap-[3vh]  '  action="">
+                <form onSubmit={(e)=>{handlesubmit(e)}} className=' formy h-[80%] w-[70%] bg-blue-900 rounded-xl flex flex-col gap-[3vh]'>
                     <p className=' info text-center text-white text-lg pt-[6vh]' >Sign in / Register</p>
                     <div className='flex flex-col justify-center items-center gap-[3vh]' >
 
                         <div className=' box flex flex-col justify-center items-center gap-[3vh] w-fit bg-blue-800 p-[4vh] rounded-md ' >
                             <label className='text-white  '>
-                                <span>Org.ID : </span><input className=' inputs bg-zinc-300 rounded-md' placeholder='  Enter ID' type="text" />
+                                <span>Org.ID : </span><input onChange={(e)=>{setlogin(e.target.value)}} value={login} className=' inputs text-black bg-zinc-300 rounded-md' placeholder='  Enter ID' type="text" />
                             </label>
 
-                            <button onClick={()=>{navigate('/login')}} className='bg-zinc-200 w-[8vh] rounded-full h-[3vh] ' >Sign in</button>
+                            <button className='bg-zinc-200 w-[8vh] rounded-full h-[3vh] ' >Sign in</button>
                         </div>
 
                     </div>
@@ -35,12 +53,12 @@ const FirstPage = () => {
                     <div className='flex flex-col justify-center items-center gap-[3vh]' >
                         <div className=' box flex flex-col justify-center items-center gap-[3vh] w-fit bg-blue-800 p-[4vh] rounded-md ' >
                             <label className='text-white' >
-                                <span>Org.Name : </span><input className=' inputs bg-zinc-300 rounded-md' placeholder='  Enter Org. Name' type="text" />
+                                <span>Org.Name : </span><input onChange={(e)=>{setorgname(e.target.value)}} value={orgname} className=' inputs text-black bg-zinc-300 rounded-md' placeholder='  Enter Org. Name' type="text" />
                             </label>
                             <label className='text-white'>
-                                <span>Org.Locat. : </span><input className=' inputs bg-zinc-300 rounded-md' placeholder='  Enter Org. Location' type="text" />
+                                <span>Org.Locat. : </span><input onChange={(e)=>{setorgloc(e.target.value)}} value={orgloc} className=' inputs text-black bg-zinc-300 rounded-md' placeholder='  Enter Org. Location' type="text" />
                             </label>
-                            <button onClick={()=>{navigate('/login')}} className='bg-zinc-200 w-[9vh] h-[3vh] rounded-full' >Register</button>
+                            <button className='bg-zinc-200 w-[9vh] h-[3vh] rounded-full' >Register</button>
                         </div>
                     </div>
 
--- src/Components/ThirdPage/ThirdPage.jsx
+++ src/Components/ThirdPage/ThirdPage.jsx
@@ -1,76 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Back from '../Back/Back'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 import './ThirdPage.css'
 
 const ThirdPage = () => {
     const navigate = useNavigate();
-  return (
-    <div className='h-screen w-full relative flex justify-center items-center ' >
-        <Back/>
-        <div className='maindiv h-[80%] w-[50%] flex flex-col justify-center items-center ' >
-            <div className='h-[1px] w-[60%] bg-zinc-300 lining '></div>
-            <form className=' formidiv w-[60%] flex flex-col gap-[3vh] pl-[7vw] py-[3vw] justify-center text-white ' action="">
+    const [name, setName] = useState('');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState('');
+    const [bloodGroup, setBloodGroup] = useState('');
+    const [userdata, setuserData] = useState({});
 
-                <label>
-                    <span>Name : </span><input className='text-black ml-[1vw] rounded-md' placeholder=' Enter Name' type="text" />
-                </label>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-                <label>
-                    <span>Gender : </span>
-                    <select className='text-black rounded-md ml-[0.4vw] ' name="" id="">
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Others">Others</option>
-                    </select>
-                </label>
+        setuserData({
+            name: name,
+            gender: gender,
+            age: age,
+            bloodGroup: bloodGroup
+        });
+        setName('');
+        setGender('');
+        setAge('');
+        setBloodGroup('');
+        console.log(userdata);
+        navigate('/individual-register');
+    }
 
-                <label>
-                    <span>Age : </span><input className='w-[3vw] ageinput text-black rounded-md ml-[2vw] ' type="number" placeholder='  Age' />
-                </label>
+    return (
+        <div className='h-screen w-full relative flex justify-center items-center ' >
+            <Back />
+            <div className='maindiv h-[80%] w-[50%] flex flex-col justify-center items-center ' >
+                <div className='h-[1px] w-[60%] bg-zinc-300 lining '></div>
+                <form onSubmit={(e) => handleSubmit(e)} className='formidiv w-[60%] flex flex-col gap-[3vh] pl-[7vw] py-[3vw] justify-center text-white ' action="">
 
-                <label>
-                    <span>Bld Gr : </span>
-                    <select className=' text-black bldinput rounded-md ml-[0.9vw] w-[3vw] ' name="" id="">
-                        <option value="A+">A+</option>
-                        <option value="A-">A-</option>
-                        <option value="B+">B+</option>
-                        <option value="B-">B-</option>
-                        <option value="AB+">AB+</option>
-                        <option value="AB-">AB-</option>
-                        <option value="O+">O+</option>
-                        <option value="O-">O-</option>
-                    </select>
-                </label>
+                    <label>
+                        <span>Name : </span><input onChange={(e) => setName(e.target.value)} value={name} className='text-black ml-[1vw] rounded-md' placeholder=' Enter Name' type="text" />
+                    </label>
 
-                <button onClick={()=>{navigate('/individual-register')}} className='bg-white w-[5vw] registerbutton rounded-full text-black '>Register</button>
+                    <label>
+                        <span>Gender : </span>
+                        <select onChange={(e) => setGender(e.target.value)} value={gender} className='text-black rounded-md ml-[0.4vw] ' name="" id="">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Others">Others</option>
+                        </select>
+                    </label>
 
-            </form>
+                    <label>
+                        <span>Age : </span><input onChange={(e) => setAge(e.target.value)} value={age} className='w-[3vw] ageinput text-black rounded-md ml-[2vw] ' type="number" placeholder='  Age' />
+                    </label>
 
-            <div className='h-[1px] w-[60%] bg-zinc-300 lining '></div>
+                    <label>
+                        <span>Bld Gr : </span>
+                        <select onChange={(e) => setBloodGroup(e.target.value)} value={bloodGroup} className=' text-black bldinput rounded-md ml-[0.9vw] w-[3vw] ' name="" id="">
+                            <option value="A+">A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
+                        </select>
+                    </label>
 
-        </div>
+                    <button className='bg-white w-[5vw] registerbutton rounded-full text-black '>Register</button>
 
-        <div className='h-[80%] w-[1px] bg-white bar '></div>
+                </form>
 
-        <div className=' sideediv h-[80%] w-[50%] flex flex-col justify-around items-center  text-white '>
-            
-            <div className='w-full herotext flex flex-col gap-[2vh] justify-center items-center'>
-                <p className='  text-6xl'>Thanks for registering</p>
-                <p className=' text-6xl' >with us</p>
+                <div className='h-[1px] w-[60%] bg-zinc-300 lining '></div>
+
             </div>
 
-            <div className='flex clickhere justify-center items-center gap-[2vh]'>
-                <p>To proceed with individual details : </p>
-                <button onClick={()=>{navigate('/img-upload')}} className='bg-white clickherebutton text-black px-[1vh] rounded-full' href="">Click here</button>
+            <div className='h-[80%] w-[1px] bg-white bar '></div>
+
+            <div className='sideediv h-[80%] w-[50%] flex flex-col justify-around items-center  text-white '>
+
+                <div className='w-full herotext flex flex-col gap-[2vh] justify-center items-center'>
+                    <p className='  text-6xl'>Thanks for registering</p>
+                    <p className=' text-6xl' >with us</p>
+                </div>
+
+                <div className='flex clickhere justify-center items-center gap-[2vh]'>
+                    <p>To proceed with individual details : </p>
+                    <button onClick={() => { navigate('/img-upload') }} className='bg-white clickherebutton text-black px-[1vh] rounded-full' href="">Click here</button>
+                </div>
+
             </div>
 
-
         </div>
-
-
-    </div>
-  )
+    )
 }
 
 export default ThirdPage
